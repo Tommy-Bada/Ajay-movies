d80929eb288fe0fbca1e8820abcd81985be2510f
Bada tomiwa <tomiwabd@gmail.com>
2023-02-19 20:54:56 +0100

    api correction

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [movies, setMovies] = useState([])
   const [searchValue, setSearchValue] = useState('')
   const getMovieRequest = async (searchValue)=> {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=965d21ae`;
+    const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=965d21ae`;
     const response = await fetch(url);
     const responseJson = await response.json();
     if(responseJson.Search){
